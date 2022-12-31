865893b40a9ba2963dde4363d68144d4f26b1570
Dariussrz83 <120649002+Dariussrz83@users.noreply.github.com>
2022-12-31 15:56:25 -0300

    Nuevo mvt

---
+++ family/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Family(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.FloatField()
+    minor = models.BooleanField()
---
+++ family/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from family.models import Family
+
+
+def create_family(request):
+   new_family = Family.objects.create()
+   print(new_family)
+   return HttpResponse('se creo un nuevo miembro')
+
+def list_members(request):
+    all_members = Family.objects.all()
+    print(all_members)
+    context = {
+        'family':all_members
+    }
+    return render(request,'list_members.html',context=context)
